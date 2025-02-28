9144eb1ee0406ebf2642eaf60156f4f57ea850bb
Gabriel Macaúbas <gabrielmacaubas@outlook.com>
2025-02-28 09:53:36 -0300

    Update barber_shop.py

--- api/serializers/barber_shop.py
+++ api/serializers/barber_shop.py
@@ -3,6 +3,11 @@ from api.models import BarberShop
 
 
 class BarberShopSerializer(serializers.ModelSerializer):
+    tags = serializers.SerializerMethodField()
+
     class Meta:
         model = BarberShop
         fields = '__all__'
+
+    def get_tags(self, obj):
+        return [{"id": tag.id, "name": tag.name} for tag in obj.tags.all()]
